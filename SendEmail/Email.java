c69b45ebd6ccec996add034cbd094471157ea2dc
VitalyLesnih <mastercreative@yandex.ru>
2019-02-23 18:45:54 +0300

    SendEmail
    SendEmail

---
+++ SendEmail/Email.java
@@ -0,0 +1,15 @@
+package SendEmail;
+
+public class Email {
+
+    public static void main(String[] args) {
+        String to_email = "...who_sends...@gmail.com"; // We write Email to whom we send
+        String subject_email = "New letter?"; // Email Subject
+        String mail_email = "Email Text"; // Email Text
+
+        Send sslSender = new Send("....Your_Email....@gmail.com", "....your_password_of_mail....");
+        sslSender.send(subject_email, mail_email, to_email);
+        System.out.print("Email sent");
+    }
+
+}
---
+++ SendEmail/SendEmail.java
@@ -0,0 +1,49 @@
+package SendEmail;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class SendEmail {
+    String username_gmail;
+    String password_gmail;
+    Properties properties;
+
+    public Send(String username_gmail, String password_gmail) {
+        this.username_gmail = username_gmail;
+        this.password_gmail = password_gmail;
+
+        properties = new Properties();
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.socketFactory.port", "465");
+        properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.port", "465");
+    }
+
+    public void send(String subject_email, String text_email, String to_email){
+        Session current_session = Session.getDefaultInstance(properties, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username_gmail, password_gmail);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(current_session);
+            // Sender of the letter
+            message.setFrom(new InternetAddress(username_gmail));
+            // To whom we send the letter
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to_email));
+            // Subject of the message
+            message.setSubject(subject_email);
+            // The text of the letter itself
+            message.setText(text_email);
+
+            // we send the message
+            Transport.send(message);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
